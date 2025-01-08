06419eae46a9069d9fa94ac490708aefd2f51a7f
Pavel Mazniker <60963077+goodengineer@users.noreply.github.com>
2025-01-07 21:05:48 -0900

    Update simulateScenario.m

--- MPC virtual lab/helperfunctions/simulateScenario.m
+++ MPC virtual lab/helperfunctions/simulateScenario.m
@@ -1,6 +1,9 @@
 function simulateScenario(simulationTime,waypoints,Vx,roadLength)
 
 sampleTime = 0.1;
+vsp = 15;
+simlatency = 0.1; %increase for slower sim
+
 scenario = drivingScenario("SampleTime",0.1);
 simTime = (0:sampleTime:simulationTime)';  
 roadCenters = [-15 2 0; roadLength 2 0];
@@ -9,8 +12,7 @@ lnspec = lanespec(2,'Marking',dashW,'Width',4);
 road(scenario, roadCenters,'Lanes',lnspec);
 egoVehicle = vehicle(scenario,'ClassID',1,'Position',[0 0 0]);
 
-speed = 15;
-smoothTrajectory(egoVehicle,waypoints,speed)
+smoothTrajectory(egoVehicle,waypoints,vsp)
 
 plot(scenario);
 set(gcf, 'Visible','on')
@@ -22,7 +24,7 @@ ButtonH=uicontrol('Style','pushbutton',...
     
     function playButton(src,event,scenario)
         while advance(scenario)
-            pause(0.1) %increase for slower sim
+            pause(simlatency) 
         end
     end
 
